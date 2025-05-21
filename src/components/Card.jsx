d0cb7de0eb9744e9886cd66cc57473cdcd06f936
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-05-21 01:30:19 +0100

    fallback images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,6 @@ export default function Card({ item, onClick, config }) {
     imageField = "Logo",
     titleField = "Company Name",
     descriptionField = "Company Summary",
-    fields = [],
   } = config
 
   console.log("📦 item.fields", item.fields)
@@ -25,23 +24,21 @@ export default function Card({ item, onClick, config }) {
             className="rounded-md object-contain h-[70px] w-[70px]"
           />
         ) : (
-          <div className="h-[70px] w-[70px] bg-[#555] rounded-md" />
+          <Image
+            src="/portal.png"
+            alt={item.fields[titleField]}
+            width={70}
+            height={70}
+            className="rounded-md object-contain h-[70px] w-[70px]"
+          />
         )}
         <h3 className="text-2xl font-bold group-hover:text-[#f5f5f5]">
           {item.fields[titleField]}
         </h3>
       </div>
-      <p className="text-base mb-6 line-clamp-4 group-hover:text-[#f5f5f5]">
+      <p className="text-base line-clamp-4 group-hover:text-[#f5f5f5]">
         {item.fields[descriptionField] || "No description available."}
       </p>
-      <div className="text-base space-y-1">
-        {fields.map((field) => (
-          <p key={field} className="group-hover:text-[#f5f5f5]">
-            <strong>{field.replace(/\s?\(.*?\)/, "")}:</strong>{" "}
-            {item.fields[field] || "—"}
-          </p>
-        ))}
-      </div>
     </div>
   )
 }
