a0c2a5ee5798e325f682cb4fd6c468c29f4f38c0
João Francisco Reis <joaofranciscoreis13@outlook.com>
2025-05-21 01:27:55 +0100

    add icons

---
+++ src/common/icons.jsx
@@ -0,0 +1,121 @@
+import aerodromeIcon from "public/icons/aerodromeIcon.svg"
+import airFreightIcon from "public/icons/airFreightIcon.svg"
+import airportIcon from "public/icons/airportIcon.svg"
+import airTrafficIcon from "public/icons/airTrafficIcon.svg"
+import assetsIcon from "public/icons/assetsIcon.svg"
+import broadbandIcon from "public/icons/broadbandIcon.svg"
+import buyersIcon from "public/icons/buyersIcon.svg"
+import certificationIcon from "public/icons/certificationIcon.svg"
+import companyIcon from "public/icons/companyIcon.svg"
+import demographicIcon from "public/icons/demographicIcon.svg"
+import droneRegulationIcon from "public/icons/droneRegulationIcon.svg"
+import employmentIcon from "public/icons/employmentIcon.svg"
+import environmentIcon from "public/icons/environmentIcon.svg"
+import exclamationIcon from "public/icons/exclamationIcon.svg"
+import frameworkIcon from "public/icons/frameworkIcon.svg"
+import governmentSpendingIcon from "public/icons/governmentSpendingIcon.svg"
+import greenIcon from "public/icons/greenIcon.svg"
+import heliportIcon from "public/icons/heliportIcon.svg"
+import incomeIcon from "public/icons/incomeIcon.svg"
+import infoIcon from "public/icons/infoIcon.svg"
+import infrastructureIcon from "public/icons/infrastructureIcon.svg"
+import infrastructureMetricsIcon from "public/icons/infrastructureMetricsIcon.svg"
+import internationalIcon from "public/icons/internationalIcon.svg"
+import keyRegulatorIcon from "public/icons/keyRegulatorIcon.svg"
+import laborIcon from "public/icons/laborIcon.svg"
+import liabilitiesIcon from "public/icons/liabilitiesIcon.svg"
+import locationIcon from "public/icons/locationIcon.svg"
+import lostMoneyIcon from "public/icons/lostMoneyIcon.svg"
+import mobileIcon from "public/icons/mobileIcon.svg"
+import moneyBillIcon from "public/icons/moneyBillIcon.svg"
+import motorwayIcon from "public/icons/motorwayIcon.svg"
+import noisePollutionIcon from "public/icons/noisePollutionIcon.svg"
+import ordersIcon from "public/icons/ordersIcon.svg"
+import passengerIcon from "public/icons/passengerIcon.svg"
+import passengerPlaneIcon from "public/icons/passengerPlaneIcon.svg"
+import patentIcon from "public/icons/patentIcon.svg"
+import percentageIcon from "public/icons/percentageIcon.svg"
+import personIcon from "public/icons/personIcon.svg"
+import planeSpendIcon from "public/icons/planeSpendIcon.svg"
+import populationDensityIcon from "public/icons/populationDensityIcon.svg"
+import publicTransportIcon from "public/icons/publicTransportIcon.svg"
+import questionIcon from "public/icons/questionIcon.svg"
+import railwayIcon from "public/icons/railwayIcon.svg"
+import rankingIcon from "public/icons/rankingIcon.svg"
+import rdIcon from "public/icons/rdIcon.svg"
+import regulatoryIcon from "public/icons/regulatoryIcon.svg"
+import researchCentreIcon from "public/icons/researchCentreIcon.svg"
+import researchMetricsIcon from "public/icons/researchMetricsIcon.svg"
+import revenueIcon from "public/icons/revenueIcon.svg"
+import spendingIcon from "public/icons/spendingIcon.svg"
+import spentPerVisitIcon from "public/icons/spentPerVisitIcon.svg"
+import taxIcon from "public/icons/taxIcon.svg"
+import totalPopulationIcon from "public/icons/totalPopulationIcon.svg"
+import tourismIcon from "public/icons/tourismIcon.svg"
+import towerCellIcon from "public/icons/towerCellIcon.svg"
+import vertiportIcon from "public/icons/vertiportIcon.svg"
+import visitorsIcon from "public/icons/visitorsIcon.svg"
+import wifiIcon from "public/icons/wifiIcon.svg"
+
+const icons = {
+  keyRegulatorIcon: keyRegulatorIcon,
+  frameworkIcon: frameworkIcon,
+  certificationIcon: certificationIcon,
+  noisePollutionIcon: noisePollutionIcon,
+  environmentIcon: environmentIcon,
+  droneRegulationIcon: droneRegulationIcon,
+  towerCellIcon: towerCellIcon,
+  airTrafficIcon: airTrafficIcon,
+  taxIcon: taxIcon,
+  populationDensityIcon: populationDensityIcon,
+  visitorsIcon: visitorsIcon,
+  spendingIcon: spendingIcon,
+  spentPerVisitIcon: spentPerVisitIcon,
+  tourismIcon: tourismIcon,
+  rankingIcon: rankingIcon,
+  publicTransportIcon: publicTransportIcon,
+  totalPopulationIcon: totalPopulationIcon,
+  laborIcon: laborIcon,
+  personIcon: personIcon,
+  planeSpendIcon: planeSpendIcon,
+  rdIcon: rdIcon,
+  employmentIcon: employmentIcon,
+  infrastructureIcon: infrastructureIcon,
+  aerodromeIcon: aerodromeIcon,
+  airportIcon: airportIcon,
+  heliportIcon: heliportIcon,
+  vertiportIcon: vertiportIcon,
+  locationIcon: locationIcon,
+  mobileIcon: mobileIcon,
+  broadbandIcon: broadbandIcon,
+  wifiIcon: wifiIcon,
+  motorwayIcon: motorwayIcon,
+  railwayIcon: railwayIcon,
+  passengerIcon: passengerIcon,
+  airFreightIcon: airFreightIcon,
+  passengerPlaneIcon: passengerPlaneIcon,
+  patentIcon: patentIcon,
+  percentageIcon: percentageIcon,
+  internationalIcon: internationalIcon,
+  companyIcon: companyIcon,
+  researchCentreIcon: researchCentreIcon,
+  infoIcon: infoIcon,
+  regulatoryIcon: regulatoryIcon,
+  demographicIcon: demographicIcon,
+  infrastructureMetricsIcon: infrastructureMetricsIcon,
+  researchMetricsIcon: researchMetricsIcon,
+  governmentSpendingIcon: governmentSpendingIcon,
+  greenIcon: greenIcon,
+  buyersIcon: buyersIcon,
+  moneyBillIcon: moneyBillIcon,
+  lostMoneyIcon: lostMoneyIcon,
+  revenueIcon: revenueIcon,
+  ordersIcon: ordersIcon,
+  assetsIcon: assetsIcon,
+  liabilitiesIcon: liabilitiesIcon,
+  incomeIcon: incomeIcon,
+  questionIcon: questionIcon,
+  exclamationIcon: exclamationIcon,
+}
+
+export default icons
